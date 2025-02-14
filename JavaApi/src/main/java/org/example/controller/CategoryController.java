912f247f6234d6353ce1c5f85445dc0f5ae76590
Semen_kw90@student.itstep.org <Semen_kw90@student.itstep.org>
2025-02-14 17:35:47 +0200

    JavaProject

---
+++ JavaApi/src/main/java/org/example/controller/CategoryController.java
@@ -0,0 +1,46 @@
+package org.example.controller;
+
+import org.example.dto.category.CategoryCreateDTO;
+import org.example.dto.category.CategoryEditDTO;
+import org.example.entites.CategoryEntity;
+import org.example.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<CategoryEntity> getAllCategories() {
+        return categoryService.getList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryEntity> getCategoryById(@PathVariable Integer id) {
+        Optional<CategoryEntity> category = categoryService.getById(id);
+        return category.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public CategoryEntity createCategory(@RequestBody CategoryCreateDTO dto) {
+        return categoryService.create(dto);
+    }
+
+    @PutMapping
+    public CategoryEntity editCategory(@RequestBody CategoryEditDTO dto) {
+        return categoryService.edit(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable int id) {
+        categoryService.delete(id);
+    }
+}
---
+++ JavaApi/src/main/java/org/example/service/CategoryService.java
@@ -0,0 +1,50 @@
+package org.example.service;
+
+import org.example.dto.category.CategoryCreateDTO;
+import org.example.dto.category.CategoryEditDTO;
+import org.example.entites.CategoryEntity;
+import org.example.repository.ICategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private ICategoryRepository repository;
+
+    public List<CategoryEntity> getList() {
+        return repository.findAll();
+    }
+
+    public Optional<CategoryEntity> getById(Integer id) {
+        return repository.findById(id);
+    }
+
+    public CategoryEntity create(CategoryCreateDTO dto) {
+        CategoryEntity entity = new CategoryEntity();
+        entity.setName(dto.getName());
+        entity.setImage(dto.getImage());
+        entity.setDescription(dto.getDescription());
+        entity.setCreationTime(LocalDateTime.now());
+        repository.save(entity);
+        return entity;
+    }
+
+    public CategoryEntity edit(CategoryEditDTO dto) {
+        CategoryEntity entity = repository.findById(dto.getId()).get();
+        entity.setName(dto.getName());
+        entity.setImage(dto.getImage());
+        entity.setDescription(dto.getDescription());
+        repository.save(entity);
+        return entity;
+    }
+
+    public void delete(int id) {
+        CategoryEntity entity = repository.findById(id).get();
+        repository.delete(entity);
+    }
+}
